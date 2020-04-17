e64905631fb75fafd04f2b0ffa3717c3ab76cbd8
Lauro Weingartner Neto <laurowrn@gmail.com>
2020-04-17 10:13:41 -0300

    Primeiro Commit

---
+++ src/ui/ContactForm.java
@@ -0,0 +1,43 @@
+package ui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ContactForm extends JFrame {
+
+    private JPanel rootPanel;
+    private JTextField textName;
+    private JTextField textPhone;
+    private JButton buttonCancel;
+    private JButton buttonSave;
+
+    public ContactForm() {
+        setContentPane(rootPanel);
+        setSize(500, 250);
+        setVisible(true);
+
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        setLocation(dim.width/2 - getSize().width/2, dim.height/2 - getSize().height/2);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        setListeners();
+    }
+    private void setListeners(){
+        buttonCancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new MainForm();
+                dispose();
+            }
+        });
+
+        buttonSave.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
